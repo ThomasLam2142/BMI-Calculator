07d96d92a2fdb07a09e85465327ac49de3e7f201
ThomasLam2142 <thomaslam2142@gmail.com>
2023-09-12 11:27:13 -0400

    Button routed

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-let weight = document.querySelector(".");
+let weight = document.querySelector(".inputWeight");
+let height = document.querySelector(".inputHeight");
+let bmiElement = document.querySelector(".bmiElement").value;
+
+const calculateBtn = document.querySelector(".calculateButton");
+
+
+calculateBtn.addEventListener("click", ()=>{
+
+ 
+    console.log("123123123");
+
+    //bmiElement.textContent = 100;
+
+    
+})
